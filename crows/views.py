b252146cf6b31db2e17395badf6cbb8094a04518
Sakib Jalal <sakib.jalal@gmail.com>
2016-06-18 22:01:34 -0700

    Woo sentiment works

--- crows/views.py
+++ crows/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, request, url_for
+from geopy.geocoders import Nominatim
 from crows import app, tweepy_api
-import tweepy
-
+import tweepy, requests
+# from vaderSentiment.vaderSentiment import sentiment as vaderSentiment
 
 @app.route('/', methods=['GET'])
 def index():
@@ -21,21 +22,39 @@ def current_sentiment():
 
         s = get_sentiment(lat, long, rad)
         with open('sentiment', 'w') as f:
-            f.write(s)
+            f.write(str(s))
 
         return render_template('index.html', sentiment=s)
 
 
 def get_sentiment(lat, long, rad):
     print lat, long, rad
-    #TODO: grab placeID, grab tweets, analyze sentiment of tweets
-    places = tweepy_api.reverse_geocode(lat=lat, long=long,
-        accuracy=rad*1000, granularity="city", max_results=10)
+    # grab placeID, grab tweets, analyze sentiment of tweets
+    places = tweepy_api.reverse_geocode(lat=lat, long=long, accuracy=rad*1000)
     tweets = []
     for place in places:
         for tweet in tweepy_api.search(q="place:%s" % place.id):
             tweets.append(tweet)
-        print place.name
-    print len(tweets)
-    # for tweet in tweets: print tweet.text + "\n"
-    return "weed"
+
+    geolocator = Nominatim()
+    location = geolocator.reverse("{0}, {1}".format(lat, long))
+
+    print "------------"
+    neg, neutral, pos = 0.0, 0.0, 0.0
+    for tweet in tweets:
+        print tweet.text
+        # uni_text = u' '.join((tweet.text)).encode('utf-8')
+        # vs = vaderSentiment(uni_text)
+        # print "\t" + str(vs) + "\n"
+        r = requests.post('http://text-processing.com/api/sentiment/',
+            data={'text': tweet.text}).json()["probability"]
+        neg += r["neg"]
+        neutral += r["neutral"]
+        pos += r["pos"]
+        print r["neg"], r["neutral"], r["pos"]
+        print "\n"
+
+    return {"place": location.address,
+            "neg": neg/len(tweets),
+            "neutral": neutral/len(tweets),
+            "pos": pos/len(tweets)}
